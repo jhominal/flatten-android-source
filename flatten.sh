45c56e29bea80d65a0c6331dd2e87f9d72ff551c
jhominal <jhominal@gmail.com>
2010-12-06 11:46:44 +0100

    Remove most semicolons from script.

--- flatten.sh
+++ flatten.sh
@@ -1,46 +1,42 @@
 #!/bin/sh
-tag=$1;
-source_dir=$2;
-destination_dir=$3;
+tag=$1
+source_dir=$2
+destination_dir=$3
 
 #Keep a few directory variables so that everything will go smoothly in any case
-working_dir=`pwd`;
-flatten_base_dir=`dirname $0`;
+working_dir=`pwd`
+flatten_base_dir=`dirname $0`
 
 echo "Downloading (if necessary) the affected projects, and checkouting tag '$tag' in them."
 
-while read project ;
-do 
-  if [ ! -d $source_dir/$project ]
-  then
-    mkdir -p $source_dir/$project ;
+while read project ; do 
+  if [ ! -d $source_dir/$project ] ; then
+    mkdir -p $source_dir/$project
   fi
-  cd $source_dir/$project ;
-  if [ ! -d .git ]
-  then
+  cd $source_dir/$project
+  if [ ! -d .git ] ; then
     cd ..
-    git clone -o korg git://android.git.kernel.org/platform/$project.git ;
-    cd `basename $project` ;
-    git fetch ;
+    git clone -o korg git://android.git.kernel.org/platform/$project.git
+    cd `basename $project`
+    git fetch
   fi
-  git checkout $tag ;
-  cd $working_dir ;
+  git checkout $tag
+  cd $working_dir
 done < $flatten_base_dir/$tag.projects
 
-mkdir -p $destination_dir;
+mkdir -p $destination_dir
 
-echo "Copying files...";
+echo "Copying files..."
 
-while read folder ;
-do cp -r $source_dir/$folder/* $destination_dir ;
+while read folder ; do 
+  cp -r $source_dir/$folder/* $destination_dir
 done < $flatten_base_dir/$tag.folders
 
 #Remove Android.mk - it does not make sense in that context.
-rm -f $destination_dir/Android.mk ;
+rm -f $destination_dir/Android.mk
 
 #Put the resources in their rightful package
-cp -r $destination_dir/resources/* $destination_dir ;
+cp -r $destination_dir/resources/* $destination_dir
 rm -rf $destination_dir/resources
 
-echo "Done.";
-
+echo "Done."
