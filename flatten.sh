a0c49f543b22b88126520ed37a40272c09791a4e
jhominal <jhominal@gmail.com>
2010-12-06 01:12:02 +0100

    Add korg/master branch for good measure.

--- flatten.sh
+++ flatten.sh
@@ -9,19 +9,18 @@ flatten_base_dir=`dirname $0`;
 
 echo "Downloading (if necessary) the affected projects, and checkouting tag '$tag' in them."
 
-while read line ;
+while read project ;
 do 
-  if [ ! -d $source_dir/$line ]
+  if [ ! -d $source_dir/$project ]
   then
-    mkdir -p $source_dir/$line ;
+    mkdir -p $source_dir/$project ;
   fi
-  cd $source_dir/$line ;
+  cd $source_dir/$project ;
   if [ ! -d .git ]
   then
     cd ..
-    git clone -o korg git://android.git.kernel.org/platform/$line.git ;
-    cd $working_dir ;
-    cd $source_dir/$line ;
+    git clone -o korg git://android.git.kernel.org/platform/$project.git ;
+    cd `basename $project` ;
     git fetch ;
   fi
   git checkout $tag ;
@@ -32,14 +31,16 @@ mkdir -p $destination_dir;
 
 echo "Copying files...";
 
-while read line ;
-do cp -r $source_dir/$line/* $destination_dir ;
+while read folder ;
+do cp -r $source_dir/$folder/* $destination_dir ;
 done < $flatten_base_dir/$tag.folders
 
-#Remove files that don't make sense in a flattened hierarchy.
+#Remove Android.mk - it does not make sense in that context.
 rm -f $destination_dir/Android.mk ;
-#Put the "resources" in their rightful package
+
+#Put the resources in their rightful package
 cp -r $destination_dir/resources/* $destination_dir ;
 rm -rf $destination_dir/resources
+
 echo "Done.";
 
